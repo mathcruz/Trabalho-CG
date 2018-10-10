4c67f8ae229807d6f21079d1467ce75989704e8c
ramonrrezende <ramonrocharezende@gmail.com>
2018-10-10 13:38:39 -0300

    Introdução da animação

--- main.py
+++ main.py
@@ -1,83 +1,39 @@
-# Import a library of functions called 'pygame'
 import pygame
 import numpy as np
 import math
-
-# Initialize the game engine
 pygame.init()
 
-# Define the colors we will use in RGB format
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+# Definicao das cores (valores RGB)
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
 
-# Set the height and width of the screen
-size = [1000, 1000]
-screen = pygame.display.set_mode(size)
+# Iniciando os valores do SRD (Sistema de Referencia do Display)
+SRD = [400, 300]
+tela = pygame.display.set_mode(SRD)
 
-pygame.display.set_caption("Example code for the draw module")
+limiteMin = [0,0]       #[xMin, yMin]
+limiteMax = [100,100]   #[xMax. yMax]
 
-# Loop until the user clicks the close button.
-done = False
+
+#funcao que mapeia as coordenadas do SRU para o SRD
+def map(pontos):
+    novosPontos = []
+    for i in range(pontos.__len__()):
+        x = pontos[i][0]
+        y = pontos[i][1]
+        novoX = x * SRD[0] / limiteMax[0]
+        novoY = -1 * y * SRD[1] / limiteMax[1] + SRD[1]
+        novosPontos.append([novoX,novoY])
+
+    return novosPontos
+
+fim = False
 clock = pygame.time.Clock()
 
-class dois():
-	def __init__(self):
-		self.pontos = [[10,00],#1
-	  [35,00],#2
-	  [50,10],#3
-	  [50,45],#4
-	  [5,60],#5
-	  [50,60],#6
-	  [50,70],#7
-	  [00,70],#8
-	  [00,53],#9
-	  [40,38],#10
-	  [40,15],#11
-      [33,10],#12
-	  [13,10],#13
-	  [5,18],#14
-	  [0,10]]#15
-
-	def draw(self):
-		for i in range(-1, len(self.pontos) - 1):
-			pygame.draw.line(screen, BLACK, self.pontos[i], self.pontos[i + 1], 1)
-
-	def translate(self, m, n):
-		if(self.pontos[0] == m and self.pontos[1] == n):
-			return
-		m = m
-		n = n
-		m_t = [[1, 0, m],
-		    [0, 1, n],
-		    [0, 0, 1]
-		]
-		for i in range(len(self.pontos)):
-			a = [[self.pontos[i][0]], [self.pontos[i][1]], [1]]
-			m_t = np.matrix(m_t)
-			a = np.matrix(a)
-			a = np.dot(m_t, a)
-			self.pontos[i][0] = a[0]
-			self.pontos[i][1] = a[1]
-	def rotate(self, ang):
-		m_t = [[math.cos(ang), math.sin(ang), 0],
-		       [-math.sin(ang), math.cos(ang), 0],
-		       [0, 0, 1]
-		]
-		for i in range(len(self.pontos)):
-			a = [[self.pontos[i][0]], [self.pontos[i][1]], [1]]
-			m_t = np.matrix(m_t)
-			a = np.matrix(a)
-			a = np.dot(m_t, a)
-			self.pontos[i][0] = a[0]
-			self.pontos[i][1] = a[1]
-
-	def animate(self, m, n, ang, t):
+def animate(pontos, m, n, ang, t):
 		ang = ang/t
-		m = m/t + self.pontos[0][0]
-		n = n/t + self.pontos[0][1]
+		m = m/t + pontos[0][0]
+		n = n/t + pontos[0][1]
 		m_tr = [[math.cos(ang), math.sin(ang), 0],
 		       [-math.sin(ang), math.cos(ang), 0],
 		       [0, 0, 1]
@@ -88,8 +44,8 @@ class dois():
 		    [0, 0, 1]
 		]
 
-		m_t0 = [[1, 0, -self.pontos[0][0]],
-		    [0, 1, -self.pontos[0][1]],
+		m_t0 = [[1, 0, -pontos[0][0]],
+		    [0, 1, -pontos[0][1]],
 		    [0, 0, 1]
 		]
 		m_ta = np.dot(m_tr, m_t0)
@@ -97,42 +53,59 @@ class dois():
 #		self.translate(0,0)
 #		self.rotate(-math.pi/2)
 #		self.translate(m, n)
-		for i in range(len(self.pontos)):
-			a = [[self.pontos[i][0]], [self.pontos[i][1]], [1]]
+		for i in range(len(pontos)):
+			a = [[pontos[i][0]], [pontos[i][1]], [1]]
 			a = np.dot(m_ta, a)
-			self.pontos[i][0] = a[0]
-			self.pontos[i][1] = a[1]
+			pontos[i][0] = a[0]
+			pontos[i][1] = a[1]
 
-fig = dois()
-fig.translate(50, 50)
-#fig.animate(200,200,0)
+
+pontos = [[0,0],
+          [0,2],
+          [6,7],
+          [2,7],
+          [1,6],
+          [0,7],
+          [0,8],
+          [1,9],
+          [7,9],
+          [8,8],
+          [8,6],
+          [3,2],
+          [8,2],
+          [8,0]]
+
+def translada(x, y, pontos):
+    novosPontos = []
+    for i in range(pontos.__len__()):
+        novoX = pontos[i][0] + x
+        novoY = pontos[i][1] + y
+        novosPontos.append([novoX,novoY])
+
+    return novosPontos
+
+pontos = translada(50, 50, pontos)
 state = 0
-max_state = 120
-#fig.animate(100,100,0)
-while not done:
+max_state = 60
+while not fim:
+    # o clock.tick(x) limita a repeticao do loop em x vezes por segundo (eh seguro deixa-lo assim)
+    clock.tick(30)
 
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
-    clock.tick(60)
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
+    for event in pygame.event.get():  # Captura as interacoes do usuario
+        if event.type == pygame.QUIT:  # Se o usuario clicou para sair..
+            fim = True  # Muda a flag que indica o fim do loop
 
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
+    # Limpa a tela e reseta o background
+    tela.fill(BRANCO)
 
-    # Clear the screen and set the screen background
-    screen.fill(WHITE)
-
-    # This draws a triangle using the polygon command
+    # Desenha um poligono usando como referencia os pontos recebidos
     if(state < max_state):
         state = state + 1
-        fig.animate(400, 400, -math.pi/2, max_state)
-    fig.draw()
+        animate(pontos, -41, 42, -math.pi/2, max_state)
+    pygame.draw.polygon(tela, PRETO, map(pontos), 1)
 
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
+    # Desenha as mudancas feitas na tela
     pygame.display.flip()
 
-# Be IDLE friendly
 pygame.quit()
+
